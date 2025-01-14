ec2af85b35f9a2621a913d5542b5b3ac29e5ed4d
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-01-15 03:55:34 +0900

    搁哈呢？

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import requests
+import fastapi
+import uvicorn
+
+def get_gist_content():
+    pass
+
+def put_gist_content():
+    pass
+
+def authentication():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
