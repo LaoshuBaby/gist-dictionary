91d55595c8ffaee13873fff1a0b95f116d5507c0
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-09 01:52:22 +0900

    feat: log

--- src/main.py
+++ src/main.py
@@ -5,6 +5,75 @@ import fastapi
 import requests
 import uvicorn
 
+import hellologger
+
+from hellologger import get_logger
+
+
+def get_yuheng_path() -> str:
+    YUHENG_PATH = os.path.join(os.environ["USERPROFILE"], ".yuheng")
+
+    def init_yuheng_path() -> None:
+        YUHENG_FOLDER = ["cache", "db_profiles", "log"]
+        YUHENG_PROFILES = [
+            "mysql.db_profiles.yuheng",
+            "postgresql.db_profiles.yuheng",
+        ]
+        # root check
+        if os.path.exists(YUHENG_PATH) != True:
+            print("[YUHENG ENVIRON INIT]", YUHENG_PATH, "isn't exist!")
+            os.mkdir(YUHENG_PATH)
+        # folder check
+        for folder in YUHENG_FOLDER:
+            this_folder_path = os.path.join(YUHENG_PATH, folder)
+            if os.path.exists(this_folder_path) != True:
+                print(
+                    "[YUHENG ENVIRON INIT]", this_folder_path, "isn't exist!"
+                )
+                os.mkdir(this_folder_path)
+        # profile files check
+        for profile in YUHENG_PROFILES:
+            this_profile_path = os.path.join(
+                YUHENG_PATH, "db_profiles", profile
+            )
+            if os.path.exists(this_profile_path) != True:
+                import json
+
+                print(
+                    "[YUHENG ENVIRON INIT]", this_profile_path, "isn't exist!"
+                )
+                with open(
+                    this_profile_path, "w", encoding="utf-8"
+                ) as f_this_profile:
+                    f_this_profile.write(
+                        json.dumps(
+                            {
+                                "yuheng_doctype": "db_profile",
+                                "_WARNING": "PLEASE DELETE THIS LINE AND FILL IT ACCORDING TO DOCS.",
+                            }
+                        )
+                    )
+
+    init_yuheng_path()
+    return YUHENG_PATH
+
+
+
+logger = get_logger(
+    log_path=os.path.join(get_yuheng_path(), "log"),
+    log_file="log_{time}.log",
+    log_target={
+        "local": True,
+        "aliyun": False,
+        "aws": False,
+    },
+    log_level={
+        "local": "TRACE",
+        "aliyun": "INFO",
+    },
+    **{**log_config_local, **log_config_aliyun},
+)
+
 CONFIG_FILENAME = ".gist_dictionary.json"
 
 
