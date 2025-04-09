48345074167fa99b0a385e48cfc37eb25e91b060
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-09 12:30:25 +0900

    refactor: BREAKING CHANGES

--- src/main.py
+++ src/main.py
@@ -2,74 +2,10 @@ import json
 import os
 
 import fastapi
-import hellologger
-import requests
 import uvicorn
-from hellologger import get_logger
 
-ROOT_PATH = os.path.join(os.environ["USERPROFILE"], ".gist_dictionary")
-
-
-ROOT_FOLDERS = ["log", "config"]
-ROOT_PROFILES = [".gist_dictionary.json"]
-
-
-def init_root_path() -> None:
-
-    # root check
-    if os.path.exists(ROOT_PATH) != True:
-        os.mkdir(ROOT_PATH)
-    # folder check
-    for folder in ROOT_FOLDERS:
-        this_folder_path = os.path.join(ROOT_PATH, folder)
-        if os.path.exists(this_folder_path) != True:
-
-            os.mkdir(this_folder_path)
-    # profile files check
-
-    for profile in ROOT_PROFILES:
-        this_profile_path = os.path.join(ROOT_PROFILES, "config", profile)
-        if os.path.exists(this_profile_path) != True:
-            import json
-
-            with open(
-                this_profile_path, "w", encoding="utf-8"
-            ) as f_this_profile:
-                f_this_profile.write(
-                    json.dumps(
-                        {
-                            "GH_TOKEN": "DO NOT CONTAIN THIS",
-                            "config": {"gist_name": ""},
-                        },
-                        ensure_ascii=False,
-                        indent=2,
-                        sort_keys=False,
-                    )
-                )
-
-
-def get_root_path() -> str:
-    init_root_path()
-    return ROOT_PATH
-
-
-logger = get_logger(
-    log_path=os.path.join(get_root_path(), "log"),
-    log_file="log_{time}.log",
-    log_target={
-        "local": True,
-        "aliyun": False,
-        "aws": False,
-    },
-    log_level={
-        "local": "TRACE",
-        "aliyun": "INFO",
-    },
-    **{"foo": "bar"},
-)
-
-
-CONFIG_FILENAME = ".gist_dictionary.json"
+from basic import CONFIG_FILENAME, ROOT_PROFILES, logger
+from environment import authentication
 
 
 def set_config():
@@ -81,25 +17,30 @@ def set_config():
     # 初次运行的时候自动生成config不属于这个函数的功能
     pass
 
-
-def init_config() -> dict:
-    config = get_config()
-    # 逻辑，启动init的时候自动执行一个检测是否有config的检测
-    # 然后去获取（这时候一定是有文件的）
-    # 如果获取到的里面有问题就要求用户填写信息。
-    # 然后整完直接整个覆盖到现有config文件上去
-    return config
-
-
 def get_config() -> dict:
-    with open(CONFIG_FILENAME, "r", encoding="utf-8") as f:
+    profile_path = os.path.join(ROOT_PROFILES, "config", CONFIG_FILENAME)
+    with open(profile_path, "r", encoding="utf-8") as f:
         config = json.loads(f.read())
         return config
     return {}
 
 
-def main():
-    # init_config=init()
+def init_config() -> dict:
+    """
+    # 逻辑，启动init的时候自动执行一个检测是否有config的检测
+    # 然后去获取（这时候一定是有文件的）
+    # 如果获取到的里面有问题就要求用户填写信息。
+    # 然后整完直接整个覆盖到现有config文件上去
+    """
+    config = get_config()
+
+    return config
+
+
+def init():
+    """
+    在经过init之后，就已经获得了authentication的token了
+    """
     logger.info("HelloWOrld")
     config = init_config()
     # print(config)
@@ -112,6 +53,12 @@ def main():
     authentication(token=TOKEN)
     # get_gist(auth_token=TOKEN,gist_id="")
     pass
+
+    pass
+
+
+def main():
+    config=init()
 
 
 if __name__ == "__main__":
