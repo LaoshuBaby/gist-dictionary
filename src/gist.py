4c84f9da978d63053e88842bb26e5481a9d9e47d
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2025-04-09 17:05:45 +0900

    feat: allow change different api

--- src/gist.py
+++ src/gist.py
@@ -10,29 +10,52 @@ from log import logger
 # 需要获取的gist名字推荐提前放在配置文件里面，建议写一个init过程
 # 这块的读取文档在 https://docs.github.com/en/rest/gists/gists?apiVersion=2022-11-28#get-a-gist
 
+IMPLEMENT="local"
+
+# Optional value:
+# "local": 
+# * impl made by me
+# "witherredaway": 
+# * PyPi-[github-gists](https://pypi.org/project/github-gists/)
+# * GitHub-[WitherredAway/gists.py](https://github.com/WitherredAway/gists.py)
+
 
 def get_gist(auth_token: str, gist_id: str):
 
-    url = f"https://api.github.com/gists/{gist_id}"
+    if IMPLEMENT == "local":
+        url = f"https://api.github.com/gists/{gist_id}"
 
-    headers = {
-        "Authorization": f"token {auth_token}",
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
+        headers = {
+            "Authorization": f"token {auth_token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
 
-    try:
-        response = requests.get(url, headers=headers)
-    except Exception as e:
-        logger.error(e)
+        try:
+            response = requests.get(url, headers=headers)
+        except Exception as e:
+            logger.error(e)
 
-    if response.status_code == 200:
-        gist_metadata = response.json()
-        logger.trace(gist_metadata)
-    else:
-        logger.error(f"Failed to retrieve gist: {response.status_code}")
-        logger.warning(response.text)
+        if response.status_code == 200:
+            gist_metadata = response.json()
+            logger.trace(gist_metadata)
+        else:
+            logger.error(f"Failed to retrieve gist: {response.status_code}")
+            logger.warning(response.text)
 
-    gist_data=gist_metadata.get("files").get("wordbank.json").get("content")
+        gist_data=gist_metadata.get("files").get("wordbank.json").get("content")
+    elif IMPLEMENT == "witherredaway":
+        import asyncio
+        import gists
+
+        client = gists.Client()
+        async def main_get():
+            # Getting a gist does not require authorization
+
+            # This method fetches the gist associated with the provided gist id, and returns a Gist object
+            gist = await client.get_gist("GIST ID")
+            return gist
+
+        gist_data = asyncio.run(main_get())
     return gist_data
 
 
